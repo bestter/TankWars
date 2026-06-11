f9c4e56cb903a22f8bd2ff41cdb59e4fecfc0a51
Martin Labelle <martin.labelle1@gmail.com>
2026-06-10 22:17:04 -0400

    Disable non-error console logs in production and bump version to 0.3.3
    Signed-off-by: Antigravity (Gemini 3.5 Flash (High))

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,22 @@ import { HelmetProvider } from 'react-helmet-async';
 import "./index.css";
 import App from "./App.tsx";
 
+// Neutralize non-error console methods in production
+if (import.meta.env.PROD) {
+  const noop = () => {};
+  console.log = noop;
+  console.info = noop;
+  console.warn = noop;
+  console.debug = noop;
+  console.trace = noop;
+  console.group = noop;
+  console.groupCollapsed = noop;
+  console.groupEnd = noop;
+  console.time = noop;
+  console.timeEnd = noop;
+}
+
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <HelmetProvider>
